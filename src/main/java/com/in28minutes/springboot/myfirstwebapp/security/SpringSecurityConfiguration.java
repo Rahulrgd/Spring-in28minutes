c5a594f832d8782a066924fdb194ad32d2917b56
RahulGupta <rahulrgd21@gmail.com>
2024-01-11 01:55:38 +0530

    Completed lecture 127, Learned about h2databases

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -18,15 +18,21 @@ public class SpringSecurityConfiguration {
 
     @Bean
     public InMemoryUserDetailsManager createUserDetailsManager(){
+        UserDetails userDetails1 = createNewUser("in28minutes", "dummy");
+        UserDetails userDetails2 = createNewUser("Rahul", "1234");
+
+        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+    }
+
+    private UserDetails createNewUser(String username, String password) {
         Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
         UserDetails userDetails = User.withDefaultPasswordEncoder()
         .passwordEncoder(passwordEncoder)
-        .username("in28minutes")
-        .password("dummy")
+        .username(username)
+        .password(password)
         .roles("USER","ADMIN")
         .build();
-
-        return new InMemoryUserDetailsManager(userDetails);
+        return userDetails;
     }
 
     @Bean

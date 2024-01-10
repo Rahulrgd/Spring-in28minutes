16f2dad058853a23761c21e9a43f2799af9e7785
RahulGupta <rahulrgd21@gmail.com>
2024-01-10 13:39:36 +0530

    Completed Lecture-115

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -3,19 +3,38 @@ package com.in28minutes.springboot.myfirstwebapp.todo;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
+    private static int todoCount = 0;
     private static List<Todo> todos = new ArrayList<>();
     static{
-        todos.add(new Todo(1, "in28minutes", "Learn AWS", LocalDate.now().plusYears(1), false));
-        todos.add(new Todo(2, "in28minutes", "Learn DevOps", LocalDate.now().plusYears(2), false));
-        todos.add(new Todo(3, "in28minutes", "Learn Full Stack Development", LocalDate.now().plusYears(3), false));
+        todos.add(new Todo(++todoCount, "in28minutes", "Learn AWS", LocalDate.now().plusYears(1), false));
+        todos.add(new Todo(++todoCount, "in28minutes", "Learn DevOps", LocalDate.now().plusYears(2), false));
+        todos.add(new Todo(++todoCount, "in28minutes", "Learn Full Stack Development", LocalDate.now().plusYears(3), false));
     }
 
     public List<Todo> findByUserName(String username){
         return todos;
     }
+
+    public void addTodo(String username, String description, LocalDate targetDate, boolean done){
+        Todo todo = new Todo(++todoCount, username, description, targetDate, done);
+        todos.add(todo);
+    }
+
+    public void deleteById(int id){
+        Predicate<? super Todo> predicate = todo->todo.getId()==id;
+        todos.removeIf(predicate);
+    }
+
+    public Todo findById(int id) {
+        Predicate<? super Todo> predicate = todo->todo.getId()==id;
+        Todo todo = todos.stream().filter(predicate).findFirst().get();
+        return todo;
+    }
+
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/todo.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/todo.jsp
@@ -1,4 +1,5 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%> <%@ taglib
+uri="http://www.springframework.org/tags/form" prefix="form" %>
 
 <html>
   <head>
@@ -11,10 +12,15 @@
   </head>
   <body>
     <div class="container">
-      <h1>Enter Todo Details: </h1>
-      <form method="POST">
-        Description: <input type="text" name="description"><input type="submit" class="btn btn-success">
-      </form>
+      <h1>Enter Todo Details:</h1>
+      <form:form method="post" modelAttribute="todo">
+        Description:
+        <form:input type="text" path="description" required="required" />
+        <form:errors path="description" cssClass="text text-warning"/> 
+        <form:input type="hidden" path="id" />
+        <form:input type="hidden" path="done" />
+        <input type="submit" class="btn btn-success" />
+      </form:form>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
